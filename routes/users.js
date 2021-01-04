63592f568027dc78413020686f48f40f76d1a7ec
PriitVallap <priitvallap@gmail.com>
2021-01-04 15:02:50 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
-/* GET users listing. */
+/* /GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});  
+
+router.get('/', function (req, res, next) {
+
+    fs.readFile('./data/users.json', (err, data) => {
+        if (err) throw err;
+        let users = JSON.parse(data);
+        res.json(users);
+    });
 });
 
 module.exports = router;
